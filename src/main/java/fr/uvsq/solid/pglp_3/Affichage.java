0357af056ee51bb8c214b62c7519b8c4881de4f4
Only_soft <abdullahwin99@gmail.com>
2020-03-04 10:04:26 +0100

    creation du projet pglp_3.5

---
+++ src/main/java/fr/uvsq/solid/pglp_3/Affichage.java
@@ -0,0 +1,6 @@
+package fr.uvsq.solid.pglp_3;
+
+public interface Affichage {
+	 public void affiche();
+
+}
---
+++ src/main/java/fr/uvsq/solid/pglp_3/Affiche.java
@@ -0,0 +1,12 @@
+package fr.uvsq.solid.pglp_3;
+
+public class Affiche implements Affichage {
+
+	public void affiche() {
+		
+		
+	}
+	
+
+
+}
